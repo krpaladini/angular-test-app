e6a889532248e8326d06f73fb28e3e6f68b5473a
Kyle Paladini <krpaladini@hotmail.com>
2018-02-22 04:09:45 +0000

    adding kyle component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { KyleComponent } from './kyle/kyle.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    KyleComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/kyle/kyle.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { KyleComponent } from './kyle.component';
+
+describe('KyleComponent', () => {
+  let component: KyleComponent;
+  let fixture: ComponentFixture<KyleComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ KyleComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(KyleComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
